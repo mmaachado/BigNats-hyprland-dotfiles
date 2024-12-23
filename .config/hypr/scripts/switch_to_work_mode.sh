66bf1f0470e606fb0bae4db95f4d6c034153c720
NathanGros <nathan.gros6@gmail.com>
2024-12-23 23:24:35 +0100

    added working waybar for both modes, with color changes and scripts

--- .config/hypr/scripts/switch_to_work_mode.sh
+++ .config/hypr/scripts/switch_to_work_mode.sh
@@ -2,3 +2,5 @@
 
 swww img ~/wallpapers/rocket_launch.jpg --transition-type outer --transition-bezier 0.48,0.64,0.65,0.06 --transition-duration 1
 cp ~/.config/hypr/conf/work_mode.conf ~/.config/hypr/conf/active_mode.conf
+cp ~/.config/waybar/work_style.css ~/.config/waybar/style.css
+source ~/.config/waybar/launch.sh
