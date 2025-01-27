618ec83fc4b1985f6ee944a957d6c83957de2cc1
NathanGros <125664066+NathanGros@users.noreply.github.com>
2025-01-27 15:09:29 +0100

    Update installupdates.sh

--- .config/hypr/scripts/installupdates.sh
+++ .config/hypr/scripts/installupdates.sh
@@ -5,25 +5,9 @@ echo
 echo "---------- UPDATE SYSTEM ----------"
 echo
 
-_isInstalled() {
-   package="$1";
-	 check="$(yay -Qs --color always "${package}" | grep "local" | grep "${package} ")";
-
-   if [ -n "${check}" ] ; then
-       echo 0; #'0' means 'true' in Bash
-       return; #true
-   fi;
-echo 1; #'1' means 'false' in Bash
-return; #false
-}
-
 aur_helper="yay"
 
 $aur_helper
-
-if [[ $(_isInstalled "flatpak") == "0" ]] ;then
-    flatpak upgrade
-fi
 
 notify-send "Update complete"
 echo 
