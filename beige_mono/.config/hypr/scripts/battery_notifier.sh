5f2238de8b2229bd96c3a2a1b1b4e2f8982002b5
NathanGros <nathan.gros6@gmail.com>
2025-02-26 12:05:48 +0100

    way better battery notifications script

--- beige_mono/.config/hypr/scripts/battery_notifier.sh
+++ beige_mono/.config/hypr/scripts/battery_notifier.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+is_notification_on=0
 while true; do
   bat_lvl=$(cat /sys/class/power_supply/BAT1/capacity)
+  is_charging=$(cat /sys/class/power_supply/ACAD/online)
   if [ "$bat_lvl" -le 20 ]; then
-    notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%\nClick to dismiss"
-    sleep 300
-  else
-    sleep 60
+    if [ "$is_charging" -eq 0 ] && [ $is_notification_on -eq 0 ]; then
+      notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%\nClick to dismiss"
+      is_notification_on=1
+    fi
+    if [ "$is_charging" -eq 1 ] && [ $is_notification_on -eq 1 ]; then
+      makoctl dismiss -a
+      is_notification_on=0
+    fi
   fi
+  sleep 10
 done
