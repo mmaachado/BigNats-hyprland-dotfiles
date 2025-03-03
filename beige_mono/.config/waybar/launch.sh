b771892f46fb602f0c0c3c9df05b5d0afdd4529f
NathanGros <nathan.gros6@gmail.com>
2025-03-04 00:09:14 +0100

    modified style and behavior of workspace buttons in waybar (now they are sick)

--- beige_mono/.config/waybar/launch.sh
+++ beige_mono/.config/waybar/launch.sh
@@ -4,4 +4,4 @@ killall waybar
 pkill waybar
 sleep 0.1
 
-waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css &
+waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style_dark.css &
