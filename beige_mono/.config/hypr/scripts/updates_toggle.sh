51c4e8fd3da62615f66ee5e8dcb8a3ce57a7fbf0
Marcelo Machado <mjoelmachadojr@gmail.com>
2025-06-02 11:20:33 -0300

    changed update script

--- beige_mono/.config/hypr/scripts/updates_toggle.sh
+++ beige_mono/.config/hypr/scripts/updates_toggle.sh
@@ -2,5 +2,5 @@
 if pgrep -x updates_install; then
   pkill updates_install
 else
-  foot -T updates -e ~/.config/hypr/scripts/updates_install.sh &
+  kitty -T updates -e ~/.config/hypr/scripts/updates_install.sh &
 fi
